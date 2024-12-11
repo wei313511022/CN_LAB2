9edc95319b7f2ef33b0e6f2073722b497552afe7
wei313511022 <tommy1213.chen@gmail.com>
2024-12-11 14:42:05 +0800

    hi

--- lab2_ryu.py
+++ lab2_ryu.py
@@ -1,20 +1,20 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.topology.api import get_switch, get_link
 from ryu.topology import event
-from ryu.lib.packet import packet, ethernet
+from ryu.lib.packet import packet, ethernet, ipv4, arp
 import networkx as nx
 
 
-class DualPathRouting(app_manager.RyuApp):
+class IPBasedRouting(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(DualPathRouting, self).__init__(*args, **kwargs)
+        super(IPBasedRouting, self).__init__(*args, **kwargs)
         self.network = nx.DiGraph()  # Graph for network topology
-        self.mac_to_port = {}        # MAC address to port mapping
+        self.ip_to_port = {}         # IP address to port mapping
         self.datapaths = {}          # Datapaths for switches
 
     @set_ev_cls(event.EventSwitchEnter)
@@ -43,47 +43,57 @@ class DualPathRouting(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
-
-        dst = eth.dst
-        src = eth.src
+        arp_pkt = pkt.get_protocol(arp.arp)
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
 
         dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
+        self.ip_to_port.setdefault(dpid, {})
 
-        # Learn the source MAC address
-        self.mac_to_port[dpid][src] = in_port
-
-        # Handle ARP packets by flooding
-        if eth.ethertype == 0x806:  # ARP packets
-            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-            out = parser.OFPPacketOut(
-                datapath=datapath, buffer_id=msg.buffer_id,
-                in_port=in_port, actions=actions, data=msg.data
-            )
-            datapath.send_msg(out)
+        # Handle ARP packets by learning and responding
+        if arp_pkt:
+            self.handle_arp(arp_pkt, datapath, in_port, parser, ofproto, msg.data)
             return
 
-        # If destination MAC is known, route the packet
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            # Compute paths and install flow rules
-            if src in self.network.nodes and dst in self.network.nodes:
-                primary_path, backup_path = self.compute_dual_paths(src, dst)
-                self.logger.info(f"Primary Path: {primary_path}")
-                self.logger.info(f"Backup Path: {backup_path}")
-                datapaths = {dp.id: dp for dp in self.datapaths.values()}
-                self.install_path(primary_path, parser, datapaths)
-                self.install_path(backup_path, parser, datapaths)
-                out_port = self.get_next_hop_port(primary_path, dpid)
-            else:
-                out_port = ofproto.OFPP_FLOOD
+        # Handle IP packets
+        if ip_pkt:
+            src_ip = ip_pkt.src
+            dst_ip = ip_pkt.dst
 
-        actions = [parser.OFPActionOutput(out_port)]
-        data = None if msg.buffer_id == ofproto.OFP_NO_BUFFER else msg.data
+            # Learn the source IP and port
+            self.ip_to_port[dpid][src_ip] = in_port
+
+            # If destination IP is known, route the packet
+            if dst_ip in self.ip_to_port[dpid]:
+                out_port = self.ip_to_port[dpid][dst_ip]
+            else:
+                # Compute paths and install flow rules
+                if src_ip in self.network.nodes and dst_ip in self.network.nodes:
+                    primary_path, backup_path = self.compute_dual_paths(src_ip, dst_ip)
+                    self.logger.info(f"Primary Path: {primary_path}")
+                    self.logger.info(f"Backup Path: {backup_path}")
+                    datapaths = {dp.id: dp for dp in self.datapaths.values()}
+                    self.install_path(primary_path, parser, datapaths)
+                    self.install_path(backup_path, parser, datapaths)
+                    out_port = self.get_next_hop_port(primary_path, dpid)
+                else:
+                    out_port = ofproto.OFPP_FLOOD
+
+            actions = [parser.OFPActionOutput(out_port)]
+            data = None if msg.buffer_id == ofproto.OFP_NO_BUFFER else msg.data
+            out = parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
+                actions=actions, data=data
+            )
+            datapath.send_msg(out)
+
+    def handle_arp(self, arp_pkt, datapath, in_port, parser, ofproto, data):
+        """
+        Handle ARP packets by learning IP-MAC mapping and flooding.
+        """
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
         out = parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
-            actions=actions, data=data
+            datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+            in_port=in_port, actions=actions, data=data
         )
         datapath.send_msg(out)
 
